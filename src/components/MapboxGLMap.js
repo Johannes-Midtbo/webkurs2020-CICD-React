c4fc58b99720da1598d961e7fe8fa83c12dad762
unknown <johannmi@stud.ntnu.no>
2020-10-21 19:13:12 +0200

    nonsenssssss

--- src/components/MapboxGLMap.js
+++ src/components/MapboxGLMap.js
@@ -28,7 +28,7 @@ const MapboxGLMap = () => {
         map.resize();
         map.addSource('kommune',{
         type: 'geojson',
-        data: "./letsDoe.geojson"});
+        data: "https://render.githubusercontent.com/view/geojson?commit=5d404195ec15558f0e322854163457e07bb20689&enc_url=68747470733a2f2f7261772e67697468756275736572636f6e74656e742e636f6d2f4a6f68616e6e65732d4d696474626f2f7765626b757273323032302d434943442d52656163742f356434303431393565633135353538663065333232383534313633343537653037626232303638392f6c657473446f652e67656f6a736f6e&nwo=Johannes-Midtbo%2Fwebkurs2020-CICD-React&path=letsDoe.geojson&repository_id=306060769&repository_type=Repository#209eb419-96f8-4d3e-bd37-121c2e3276e3"});
         map.addLayer({
           id: 'polygons',
           type: 'fill',
@@ -39,7 +39,7 @@ const MapboxGLMap = () => {
           'fill-opacity': 1.0
           },
           });
-          console.log(5 + 8);
+          console.log(5);
       });
     };
 
